824886f4c1f7eb29362134b1c4aed2765f03c789
Vishnu Vardhan <vardhanv@yahoo.com>
2018-05-13 11:54:20 -0700

    Dockerfile

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ else
    var sPort = process.argv[2]
 
 
-
+// Modify these function to do your own custom integration
 const cmdTable = {
   "ObjectCreated:Put"  : putObject,
   "ObjectCreated:Post" : unSupportedEvent,
@@ -26,20 +26,46 @@ const cmdTable = {
 
 
 
+// Modify these functions for your custom integration
+function putObject( snsRecord ) {
+    // modify to intercept puts
+    let myEvent = {
+       eventName       : snsRecord.eventName,
+       sourceIpAddress : snsRecord.requestParameters.sourceIPAddress,
+       bucketName      : snsRecord.s3.bucket.name,
+       objectKey       : snsRecord.s3.object.key
+    }
+    console.log(myEvent)
+}
+
+function delObject( snsRecord ) {
+    // modify to intercept deletes
+    let myEvent = {
+       eventName       : snsRecord.eventName,
+       sourceIpAddress : snsRecord.requestParameters.sourceIPAddress,
+       bucketName      : snsRecord.s3.bucket.name,
+       objectKey       : snsRecord.s3.object.key
+    }
+    console.log(myEvent)
+}
+
+// Unsuppported event
+function unSupportedEvent( snsRecord ) {
+    console.log("Received unSupportedEvent")
+    console.log(snsRecord)
+}
+
+
 var httpServer = http.createServer(function(req,res) {
-
    console.log("----------")
-
    let body = []
 
-   req.on('data', (chunk) => {
-      body.push(chunk)
-   })
+   req.on('data', (chunk) => { body.push(chunk) })
 
    req.on('end', () => {
 
       let resBody = "OK\n"
-      
+
       switch(req.method) {
          case 'HEAD': 
             resBody = ""
@@ -50,15 +76,10 @@ var httpServer = http.createServer(function(req,res) {
           case 'POST':
           case 'PUT': 
               let  myBody = qs.parse(decodeURIComponent(Buffer.concat(body).toString()))
-
               try { 
                   myBody.Message = JSON.parse(myBody.Message)
-
                   //console.log(util.inspect(myBody, {showHidden: false, depth: null}))
-
-                  for (let record of myBody.Message.Records) 
-                    cmdTable[record.eventName]( record )
-
+                  for (let record of myBody.Message.Records) cmdTable[record.eventName]( record )
 
                  resBody = `
                  <PublishResponse xmlns="http://vishnus-sns-server/">
@@ -72,7 +93,6 @@ var httpServer = http.createServer(function(req,res) {
                 console.log("-----" + resBody + "-----")
                 console.log(myBody)
              }
-
              break
           default:
              console.log(req.method)
@@ -88,34 +108,5 @@ var httpServer = http.createServer(function(req,res) {
 
 httpServer.listen({port: sPort})
 
-console.log ("On-prem Notifications is ready on port: ", httpServer.address().port)
+console.log ("On-Prem-SNS ready on port: ", httpServer.address().port)
 
-
-
-// Put Object Events
-
-function putObject( snsRecord ) {
-    let myEvent = {
-       eventName       : snsRecord.eventName,
-       sourceIpAddress : snsRecord.requestParameters.sourceIPAddress,
-       bucketName      : snsRecord.s3.bucket.name,
-       objectKey       : snsRecord.s3.object.key
-    }
-    console.log(myEvent)
-}
-
-function delObject( snsRecord ) {
-    let myEvent = {
-       eventName       : snsRecord.eventName,
-       sourceIpAddress : snsRecord.requestParameters.sourceIPAddress,
-       bucketName      : snsRecord.s3.bucket.name,
-       objectKey       : snsRecord.s3.object.key
-    }
-    console.log(myEvent)
-}
-
-// Unsuppported event
-function unSupportedEvent( snsRecord ) {
-    console.log("Received unSupportedEvent")
-    console.log(snsRecord)
-}
